be6d9daeb255b9488026034347cfdf6ad639c72e
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-05-06 12:40:40 -0500

    fist comit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  def index
+    @article = Article.all
+  end
 
   def new
     @article = Article.new
@@ -10,14 +14,47 @@ class ArticlesController < ApplicationController
     if @article.save
       flash[:notice] = "Article was succesfully created"
       redirect_to article_path(@article)
-    else
-      
+    else      
       render 'new'
     end
   end
 
+  def show    
+  end
+
+  def edit    
+  end
+    
+
+  def update      
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesflly updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    
+    @article.destroy
+    flash[:notice] = "Article is obliterated from the earth!!!"
+    redirect_to articles_path 
+  end
+    
   private
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+  private
+   
+  def set_article
+    @article = Article.find(params[:id])
+    
+  end
+  
+ 
 end
+
+
